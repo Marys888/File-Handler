34c9ca48530ebe48f7fc96c712d10f701ff7c509
Marys888 <severovamaha1999@gmail.com>
2025-05-08 16:49:05 +0300

    Add Application entry point implementation

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,18 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        String fileName = "contentFile.txt";
+        FileHandler fileHandler = new FileHandler();
 
+        String outputWritten = "RESULT: " + fileHandler.writeFile("I didn't know what to write here, so you're seeing this text instead.");
+        String outputRead = "FILE CONTENT: " + fileHandler.readFile(fileName);
+
+        getOutput(outputWritten);
+        getOutput(outputRead);
+    }
+
+
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
